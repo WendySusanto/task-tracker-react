7b168a982fccaedee3d37d4268d491995101d960
DESKTOP-EFVIHO8\mynam <my.name.is.wendy.susanto@gmail.com>
2022-05-29 17:06:17 +0700

    initial commit

---
+++ src/components/Task.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+//53:42 https://www.youtube.com/watch?v=w7ejDZ8SWv8
+const Task = ({ task, onDelete, onToggle }) => {
+	return (
+		<div
+			className={`task ${task.reminder ? 'reminder' : ''}`}
+			onDoubleClick={() => onToggle(task.id)}>
+			<h3>
+				{task.text}{' '}
+				<FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} />
+			</h3>
+			<p>{task.day}</p>
+			<p>
+				<Link to={`task/${task.id}`}>View Details</Link>
+			</p>
+		</div>
+	);
+};
+
+export default Task;
